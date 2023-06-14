6cf756f1ce995542724d01eaa4f1e839d0317e5e
novalrmdhn <novalrmdhn1379@gmail.com>
2023-06-14 10:49:57 +0700

    nyoba branch

--- src/main/java/id/co/indivara/jdt12/miniprojecttest2/Miniprojecttest2Application.java
+++ src/main/java/id/co/indivara/jdt12/miniprojecttest2/Miniprojecttest2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Miniprojecttest2Application {
 
 	public static void main(String[] args) {
+		System.out.println("anjay mabar");
 		SpringApplication.run(Miniprojecttest2Application.class, args);
 	}
 
