dacb8134b3260e392ee30b42f1eae6acc2ca458c
novalrmdhn <novalrmdhn1379@gmail.com>
2023-06-14 11:21:19 +0700

    balikin keawal

--- src/main/java/id/co/indivara/jdt12/miniprojecttest2/Miniprojecttest2Application.java
+++ src/main/java/id/co/indivara/jdt12/miniprojecttest2/Miniprojecttest2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Miniprojecttest2Application {
 
 	public static void main(String[] args) {
-		System.out.println("anjay mabar");
+		System.out.println("test branch lagi");
 		SpringApplication.run(Miniprojecttest2Application.class, args);
 	}
 
