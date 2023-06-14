23387626e2f46f5587322fcf3b67f3175499bb03
novalrmdhn <novalrmdhn1379@gmail.com>
2023-06-14 10:49:57 +0700

    nyoba lagi

--- src/main/java/id/co/indivara/jdt12/miniprojecttest2/Miniprojecttest2Application.java
+++ src/main/java/id/co/indivara/jdt12/miniprojecttest2/Miniprojecttest2Application.java
@@ -8,6 +8,7 @@ public class Miniprojecttest2Application {
 
 	public static void main(String[] args) {
 		System.out.println("anjay mabar");
+		System.out.println("nyoba kedua");
 		SpringApplication.run(Miniprojecttest2Application.class, args);
 	}
 
